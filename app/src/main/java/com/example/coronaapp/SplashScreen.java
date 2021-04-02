1ca3dfabda3e6fcd9da1191922a47da17c336d09
adarshdwivedi123 <ajju.alld100@gmail.com>
2021-04-02 11:46:22 +0530

    splash screen is added app complete

--- app/src/main/java/com/example/coronaapp/SplashScreen.java
+++ app/src/main/java/com/example/coronaapp/SplashScreen.java
@@ -1,14 +1,36 @@
 package com.example.coronaapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static final long SPLASH_SCREEN_TIMEOUT =2000 ;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+
+          new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+
     }
 }
